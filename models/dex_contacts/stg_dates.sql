3fbc7d5c419e2c33327b03353d31f26892601e11
Simranjeet Singh Khalsa <skhalsa@chewy.com>
2025-12-26 17:44:20 +0000

    Add dex_contacts staging models

---
+++ models/dex_contacts/stg_dates.sql
@@ -0,0 +1,7 @@
+{{ config(materialized='table') }}
+
+select
+    common_date_dttm::date as date_day
+from chewybi.common_date
+where common_date_dttm::date >= current_date - interval '3 weeks'
+  and common_date_dttm::date < current_date
