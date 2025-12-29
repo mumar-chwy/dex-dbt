fc97615f69c18cf7402c08651caf7be7e6cc5525
Simranjeet Singh Khalsa <skhalsa@chewy.com>
2025-12-29 16:57:50 +0000

    updating project

--- models/dex_contacts/stg_dates.sql
+++ models/dex_contacts/stg_dates.sql
@@ -2,6 +2,6 @@
 
 select
     common_date_dttm::date as date_day
-from chewybi.common_date
+from {{ source('chewybi','common_date')}}
 where common_date_dttm::date >= current_date - interval '3 weeks'
   and common_date_dttm::date < current_date
